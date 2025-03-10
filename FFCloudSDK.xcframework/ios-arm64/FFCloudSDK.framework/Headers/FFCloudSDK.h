fa10be08e923b13b0380be63932b69277de053bb
Soonhyung Hwang <soonhyung.hwang@jocoos.com>
2025-03-10 09:44:16 +0900

    1.0.9

---
+++ FFCloudSDK.xcframework/ios-arm64/FFCloudSDK.framework/Headers/FFCloudSDK.h
@@ -0,0 +1,18 @@
+//
+//  FFCloudSDK.h
+//  FFCloudSDK
+//
+//  Created by soonhyung on 3/4/25.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FFCloudSDK.
+FOUNDATION_EXPORT double FFCloudSDKVersionNumber;
+
+//! Project version string for FFCloudSDK.
+FOUNDATION_EXPORT const unsigned char FFCloudSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FFCloudSDK/PublicHeader.h>
+
+
